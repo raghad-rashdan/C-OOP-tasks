d45cb11afa89cd92aab52383a5707e3fa4850c84
raghad-rashdan <72078005+raghad-rashdan@users.noreply.github.com>
2022-11-27 10:09:33 +0300

    Task / methods 23.11

---
+++ november23/november23/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+//task1
+using System.Runtime.Intrinsics.X86;
+
+
+static void MyMethod()
+{
+    float sum = 0;
+    float avg = 0;
+    for (int i = 1; i <= 10; i++)
+    {
+         sum = sum + i;
+         avg = sum / i;
+    }
+
+    Console.WriteLine(" the sum of nnmber is :" + sum);
+    Console.WriteLine(" the sum of nnmber is :" + avg);
+
+}
+MyMethod();
+Console.WriteLine("--------------------------------");
+
+//task2
+
+
+
+//task3
+
+
+
+
+//task4
+
+
+//task5
+string chickens;
+chickens= Convert.ToInt32(Console.ReadLine());
