278d203ef1de62e1776245e46a4ddaa51d2cc1a6
raghad-rashdan <72078005+raghad-rashdan@users.noreply.github.com>
2022-11-28 16:43:55 +0300

    OOP class Inheritance /28.11

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Car
+    {
+        public string cars_made;
+        public int year;
+        public string type;
+        public int price;
+        public string model;
+        public int pallet_no;
+        public string color;
+
+        // Create a class constructor for the Car class
+        public Car()
+        {
+            cars_made = "metal";
+            year = 2019;
+            type = "ford";
+            price = 24000;
+            model = "Mustang";
+            pallet_no = 10;
+            color = "black";
+        }
+
+        static void MyMethod()
+        {
+            Car Ford = new Car();
+            Console.WriteLine("cars_made is :" + Ford.cars_made);
+            Console.WriteLine("year is :" + Ford.year);
+            Console.WriteLine("type is :" + Ford.type);
+            Console.WriteLine("price is :" + Ford.price);
+            Console.WriteLine("model is :" + Ford.model);
+            Console.WriteLine("pallet_no is :" + Ford.pallet_no);
+            Console.WriteLine("color is :" + Ford.color);
+        }
+        static void Main(string[] args)
+        {
+            MyMethod();
+            Car Ford = new Car();
+        }
+    }
+}
---
+++ OOP-tasks/OOP-tasks/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_tasks
+{
+class car
+    {
+        public int Year { get; set; }
+        public int Pallet_No { get; set; }
+        public double Price { get; set; }
+        public string Make { get; set; }
+        public string Type { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+         
+        public int l { get; set; }
+        public car()
+        {
+
+        }
+        public car(int year, int palletNo, double price, string carMake, string type, string model, string color)
+        {
+            Year = year;
+            Pallet_No = palletNo;
+            Price = price;
+            this.Make = carMake;
+            Type = type;
+            Model = model;
+            Color = color;
+        }
+
+
+        public void liter(int x = 10)
+        {
+
+
+            Console.WriteLine("The distance traveled in  = " + (x * 20));
+
+
+        }
+
+        public void start()
+        {
+            Console.WriteLine("The car is start ");
+        }
+        public void stop()
+        {
+            Console.WriteLine("The car is stop ");
+        }
+        public void print()
+        {
+            Console.WriteLine($"The Year of car : {Year} ");
+            Console.WriteLine($"the model : {Model}"); 
+            Console.WriteLine($" type : {Type} ");
+            Console.WriteLine($" car make {Make} ");
+            Console.WriteLine($" pallet number of car {Pallet_No} ");
+            Console.WriteLine($"  price: {Price} ");
+            Console.WriteLine($" car color :{Color} ");
+
+
+
+        }
+        class ford : car
+        {
+
+        }
+        internal class Test
+        {
+
+            static void Main(string[] args)
+            {
+
+                Console.WriteLine("Enter the year of car");
+                int year = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter the pallet number of car");
+                int palletNo = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter the price of car");
+                double price = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter the car Make of car");
+                string carMake = Console.ReadLine();
+
+                Console.WriteLine("Enter the type of car");
+                string type = Console.ReadLine();
+
+                Console.WriteLine("Enter the model of car");
+                string model = Console.ReadLine();
+
+                Console.WriteLine("Enter the color of car");
+                string color = Console.ReadLine();
+
+                car Car1 = new car(year, palletNo, price, carMake, type, model, color);
+                Car1.start();
+                Car1.stop();
+                Car1.print();
+                Car1.liter(35);
+
+                ford car = new ford();
+
+            }
+        }
+
+    }
+
+
+}
